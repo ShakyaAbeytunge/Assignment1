ddfedd075497dfd1c823100b6e1f29494a51dd71
Shakya Abeytunge <shakyaabeytunge@gmail.com>
2020-07-17 19:14:42 +0530

    Change Cipher function

--- Assignment_4/Assignment4.scala
+++ Assignment_4/Assignment4.scala
@@ -3,27 +3,22 @@ object Assignment4 extends App {
   import scala.io.StdIn.readLine
   import scala.io.StdIn.readInt
 
-  def encrypt(c: Char, s: Int): Char={
-    if(c.isLower) (97+(c-97+s)%26).toChar
+  val encrypt=(c: Char, s: Int)=> if(c.isLower) (97+(c-97+s)%26).toChar
     else (65+(c-65+s)%26).toChar
-  }
-  def decrypt(c: Char, s: Int): Char={
-    if(c.isLower) (122-(122-c+s)%26).toChar
+
+  val decrypt=(c: Char, s: Int)=> if(c.isLower) (122-(122-c+s)%26).toChar
     else (90-(90-c+s)%26).toChar
-  }
-  def convert(f:(Char,Int)=>Char,text: String, s: Int):String={
-    if(text.length==1) f(text(0),s)+"";
-    else f(text(0),s)+convert(f,text.substring(1),s)
-  }
+
+  val cipher=(f:(Char,Int)=>Char,t:String,s: Int)=>t.map(f(_,s))
 
     var text: String = readLine("Enter a text to encrypt : ");
     print("Enter shift : "); //Executes right shift
     var s: Int = readInt();
     print("Encrypted text : ");
-    println(convert(encrypt,text,s));
+    println(cipher(encrypt,text,s));
 
     text = readLine("Enter a text to decrypt : ");
     print("Decrypted text : ");
-    println(convert(decrypt,text,s));
+    println(cipher(decrypt,text,s));
 
 }
